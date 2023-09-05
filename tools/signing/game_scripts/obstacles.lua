d86d4aaced70dc502e2b5efdd71619132f077d1d
Max Weber <weber.max.1988@gmail.com>
2023-09-05 13:13:01 +0300

    Updated obstacles

--- tools/signing/game_scripts/obstacles.lua
+++ tools/signing/game_scripts/obstacles.lua
@@ -1,7 +1,7 @@
 -- this function needs to be named exactly "doObstacles" to be called by host Dart env
 function doObstacles()
   -- mkObstacle is defined by the host Dart env
-  mkObstacle(10, -1);
+  -- mkObstacle(10, -1);
   mkObstacle(15, 7);
   mkObstacle(2, 7);
   mkObstacle(-16, 2);
