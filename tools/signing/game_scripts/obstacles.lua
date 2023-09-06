4ce4572229350d1eb412aaa34a45e667ef89efd3
Max Weber <weber.max.1988@gmail.com>
2023-09-06 11:07:00 +0300

    Removed all effects

--- tools/signing/game_scripts/obstacles.lua
+++ tools/signing/game_scripts/obstacles.lua
@@ -2,15 +2,15 @@
 function doObstacles()
   -- mkObstacle is defined by the host Dart env
   -- mkObstacle(10, -1);
-  mkObstacle(15, 7);
-  mkObstacle(2, 7);
-  mkObstacle(-16, 2);
+  -- mkObstacle(15, 7);
+  -- mkObstacle(2, 7);
+  -- mkObstacle(-16, 2);
 end
 
 function doWeather()
-  mkWeather(2)
+  mkWeather(0)
 end
 
 function doDayNight()
-  mkDayNight(1);
+  mkDayNight(0);
 end
